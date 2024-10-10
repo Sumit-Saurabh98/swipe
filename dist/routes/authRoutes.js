17c0d40755adbb2d3995a888353ff052958da0c5
Sumit Saurabh <sumitsaurabh112@gmail.com>
2024-10-11 00:35:50 +0530

    Delete dist directory

--- dist/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { signup, login, logout } from "../controllers/authController.js";
-import protectRoute from '../middleware/auth.js';
-const router = express.Router();
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/logout', protectRoute, logout);
-export default router;
