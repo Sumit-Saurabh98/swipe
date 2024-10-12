386bd27037f8c208696211771ab8ef285f77b637
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-10-12 15:49:53 +0530

    added real time chat

--- client/src/store/useUserStore.ts
+++ client/src/store/useUserStore.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { AxiosError } from "axios";
-import { ErrorResponse } from "./useAuthStore";
+import { ErrorResponse, useAuthStore } from "./useAuthStore";
 import { AuthUser } from "./useAuthStore";
 
 interface UserStore {
@@ -13,10 +13,12 @@ interface UserStore {
 export const useUserStore = create<UserStore>((set) => ({
   loading: false,
 
-  updateProfile: async (name: string, age: number, bio: string, gender: string, genderPreference: string, image: string) => {
+updateProfile: async (name: string, age: number, bio: string, gender: string, genderPreference: string, image: string) => {
     try {
       set({ loading: true });
-      await axiosInstance.put<AuthUser>("/users/update-profile", { name, age, bio, gender, genderPreference, image });
+      const res = await axiosInstance.put<AuthUser>("/users/update-profile", { name, age, bio, gender, genderPreference, image });
+      useAuthStore.getState().setAuthUser(res.data.user); 
+      console.log("updating profile check: ",res.data);
       toast.success("Profile updated successfully");
     } catch (error) {
       if (error instanceof AxiosError && error.response) {
