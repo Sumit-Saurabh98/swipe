f9a5f2cf73bbc5893a3d80873ec1da052cf4e0c6
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-10-12 21:14:03 +0530

    Revert "a-2 to main branch"
    This reverts commit 1e79e1b60f9c9a7df020e605ac039f047d6d63ce.

--- src/index.ts
+++ src/index.ts
@@ -36,9 +36,9 @@ app.use('/api/matches', matchRoutes);
 app.use('/api/messages', messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "client", "dist")));
+    app.use(express.static(path.join(__dirname, "/client/build")));
     app.get("*", (req: Request, res: Response) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
 }
 
