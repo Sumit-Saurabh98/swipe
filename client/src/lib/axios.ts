e6126418b3d81087608ad73bb3154c945ce3f0bc
sumit-saurabh98 <sumitsaurabh112@gamil.com>
2025-05-29 09:41:55 +0530

    modified the

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 // TODO: update during deployment
 export const axiosInstance = axios.create({
-	baseURL: "http://localhost:5002/api",
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:8083/api" : "/api",
 	withCredentials: true,
 });
